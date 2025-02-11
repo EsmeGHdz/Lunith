3572271d41461207b29cd453e0ec04b4250fcb89
Enrique Alejandro Galván Montes <enrique.agm2177@gmail.com>
2025-02-11 16:03:40 -0600

    Register ended correctly

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,7 +1,9 @@
+import bcryptjs from 'bcryptjs';
+
 const usuarios = [{
     user: "a",
     email: "a@a",
-    password: "a"
+    password: "$2a$05$IVI38K/0GvavUcySwNUuZueN.0VLv5Y5fxanANiL0dsoDoeAtkdpO"
 }]
 
 
@@ -15,12 +17,24 @@ async function register(req, res){
     const email = req.body.email;
     const password = req.body.password;
     if(!user || !email || !password){
-        res.status(400).send({status: "Error", message: "Los campos estan incompletos"});
+        return res.status(400).send({status: "Error", message: "Los campos estan incompletos"});
     }
     const usuarioARevisar = usuarios.find(usuario => usuario.user === user);
     if(usuarioARevisar){
-        res.status(400).send({status: "Error", message: "El usuario ya existe"});
+        return res.status(400).send({status: "Error", message: "El usuario ya existe"});
     }
+    const salt = await bcryptjs.genSalt(5);
+    const hashPassword = await bcryptjs.hash(password, salt);
+    const nuevoUsuario = {
+        user,
+        email,
+        password: hashPassword
+    }
+    
+    usuarios.push(nuevoUsuario);
+    console.log(usuarios);
+    return res.status(201).send({status: "Ok", message: `Usuario ${nuevoUsuario.user} registrado`, redirect:"/login"});
+    
 }
 
 export const methods = {
