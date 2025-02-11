f9589cdbd97e72f6254fb7646b681ae9bd6110bf
Enrique Alejandro Galván Montes <enrique.agm2177@gmail.com>
2025-02-11 14:16:45 -0600

    Fixed routes, statics, public resources to get them in pages

--- app/index.js
+++ app/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 
 //Server
 const app = express();
 app.set("port", 4000);
 app.listen(app.get("port"));
 console.log("Server on port", app.get("port"));
+
+//Configuracion
+app.use(express.static(path.join(__dirname, "public")));
+
+//Rutas
+app.get("/", (req, res) => {
+    res.sendFile("pages/home.html", { root: __dirname });
+});
+
+app.get("/acercade", (req, res) => {
+    res.sendFile("pages/acercade.html", { root: __dirname });
+});
